06af41db4415f8939fe4cf8204e9727764ba5482
taiwo obembe <taiwoobembs@outlook.com>
2024-01-30 04:43:58 +0100

    updating routes and personnel controller

--- app/Http/Controllers/PersonnelController.php
+++ app/Http/Controllers/PersonnelController.php
@@ -13,7 +13,42 @@ class PersonnelController extends Controller
     {
         $personnel = Personnel::orderBy('first_name', 'ASC')->get();
         return response(['data' => $personnel, 'message' => 'personnel data', 'status' => true, 'statusCode' => env('HTTP_SERVER_CODE_OK')]);
-    
+    }
+
+    public function getById($id)
+    {
+        $personnel = Personnel::find($id);
+        if(!empty($personnel)){
+            return response(['data' => $personnel, 'message' => 'single personnel data', 'status' => true, 'statusCode' => env('HTTP_SERVER_CODE_OK')]);
+        }else{
+            return response(['data' => [], 'message' => 'unable to delete personnel data', 'status' => true, 'statusCode' => env('HTTP_SERVER_CODE_BAD_REQUEST')]);
+        }
+    }
+
+    public function create(Request $request)
+    {
+
+    }
+
+    public function update(Request $request, $id)
+    {
+        $personnel = Personnel::find($id);
+        if(!empty($personnel)){
+            return response(['data' => $personnel, 'message' => 'single personnel data', 'status' => true, 'statusCode' => env('HTTP_SERVER_CODE_OK')]);
+        }else{
+            return response(['data' => [], 'message' => 'unable to delete personnel data', 'status' => true, 'statusCode' => env('HTTP_SERVER_CODE_BAD_REQUEST')]);
+        }
+    }
+
+    public function delete($id)
+    {
+        $personnel = Personnel::find($id);
+        if(!empty($personnel)){
+            $personnel->delete();
+            return response(['data' => $personnel, 'message' => 'personnel data deleted', 'status' => true, 'statusCode' => env('HTTP_SERVER_CODE_OK')]);
+        }else{
+            return response(['data' => [], 'message' => 'unable to delete personnel data', 'status' => true, 'statusCode' => env('HTTP_SERVER_CODE_BAD_REQUEST')]);
+        }
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -20,13 +20,30 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 $prefix = "/v1/";
-Route::get($prefix . 'personnel', [PersonnelController::class, 'index']);
+// Route::get($prefix . 'personnel', [PersonnelController::class, 'index']);
 
-// Route::group(['prefix'=>'personnels','as'=>'personnel.'], function(){
-//     Route::get('/', 'PersonnelController@index')->name('index');
-//     Route::get('connect', 'PersonnelController@connect')->name('connect');
-// });
+Route::group(['prefix'=> env('PREFIX'),'as'=>'personnel'], function(){
+    Route::get('personnel/', PersonnelController::class . '@index')->name('index');
+    Route::get('personnel/{id}', PersonnelController::class . '@getById')->name('getById');
+    Route::post('personnel/', PersonnelController::class . '@create')->name('create');
+    Route::put('personnel/{id}', PersonnelController::class . '@update')->name('update');
+    Route::delete('personnel/{id}', PersonnelController::class . '@delete')->name('delete');
+});
 
+// // returns the home page with all posts
+// Route::get('/', PostController::class .'@index')->name('posts.index');
+// // returns the form for adding a post
+// Route::get('/posts/create', PostController::class . '@create')->name('posts.create');
+// // adds a post to the database
+// Route::post('/posts', PostController::class .'@store')->name('posts.store');
+// // returns a page that shows a full post
+// Route::get('/posts/{post}', PostController::class .'@show')->name('posts.show');
+// // returns the form for editing a post
+// Route::get('/posts/{post}/edit', PostController::class .'@edit')->name('posts.edit');
+// // updates a post
+// Route::put('/posts/{post}', PostController::class .'@update')->name('posts.update');
+// // deletes a post
+// Route::delete('/posts/{post}', PostController::class .'@destroy')->name('posts.destroy');
 
 // Route::get($prefix . 'getProducts', [ProductController::class, 'getProducts']);
 // Route::get($prefix . 'getProduct/{id}', [ProductController::class, 'getProduct']);
