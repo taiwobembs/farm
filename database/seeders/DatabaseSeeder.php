058cfcaf6f758ef54cba1106f08dc140255faa9e
taiwo obembe <taiwoobembs@outlook.com>
2024-01-30 17:53:30 +0100

    updated product and tool controllers

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             ResponsibilitySeeder::class,
             SupplySeeder::class,
             ProductSeeder::class,
+            ToolSeeder::class,
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PersonnelController;
 use App\Http\Controllers\SupplyController;
 use App\Http\Controllers\ResponsibilityController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ToolController;
 
 
 /*
@@ -48,4 +50,20 @@ Route::group(['prefix'=> env('PREFIX'),'as'=>'responsibility.'], function(){
     Route::post('responsibility', ResponsibilityController::class . '@create')->name('create');
     Route::put('responsibility/{id}', ResponsibilityController::class . '@update')->name('update');
     Route::delete('responsibility/{id}', ResponsibilityController::class . '@delete')->name('delete');
+});
+
+Route::group(['prefix'=> env('PREFIX'),'as'=>'product.'], function(){
+    Route::get('product', ProductController::class . '@index')->name('index');
+    Route::get('product/{id}', ProductController::class . '@getById')->name('getById');
+    Route::post('product', ProductController::class . '@create')->name('create');
+    Route::put('product/{id}', ProductController::class . '@update')->name('update');
+    Route::delete('product/{id}', ProductController::class . '@delete')->name('delete');
+});
+
+Route::group(['prefix'=> env('PREFIX'),'as'=>'tool.'], function(){
+    Route::get('tool', ToolController::class . '@index')->name('index');
+    Route::get('tool/{id}', ToolController::class . '@getById')->name('getById');
+    Route::post('tool', ToolController::class . '@create')->name('create');
+    Route::put('tool/{id}', ToolController::class . '@update')->name('update');
+    Route::delete('tool/{id}', ToolController::class . '@delete')->name('delete');
 });
